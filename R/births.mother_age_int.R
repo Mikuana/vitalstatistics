0a37e7e981e3c3fd4d96eb243e01d749d0849298
Christopher Boyd <Mikuana@gmail.com>
2017-07-02 22:48:31 -0700

    Initial draft of test

---
+++ R/births.mother_age_int.R
@@ -0,0 +1,18 @@
+#' Mother's Age at Birth
+#'
+#' A integer field which represents the mother's age at the time of birth in single year increments.
+#'
+#' ...
+#'
+#' @section Data Quality Tests:
+#'
+#' This column is tested for the following quality assumptions prior to packaging:
+#' \enumerate{
+#'   \item NA
+#' }
+#'
+#' @return a \code{\link{integer}} column
+#' @seealso \code{\link{mother_age}} \code{\link{births}}
+#' @family births-column
+#' @name mother_age_int
+NULL
