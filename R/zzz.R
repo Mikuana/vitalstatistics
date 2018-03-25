c9918c49fa64eac1c7e0432ffb290383cdb62ec4
Christopher Boyd <mikuana@gmail.com>
2018-03-24 21:26:41 -0700

    Adding data sets to global variable

--- R/zzz.R
+++ R/zzz.R
@@ -11,6 +11,10 @@ NULL
 NULL
 
 globalVariables(c(
+  "CDC_cesarean_2013",
+  "HHS_cesarean_1989",
+  "HHS_cesarean_1996",
+  "births",
   "birth_month_date",
   "birth_weekday_date",
   "birth_decade"
